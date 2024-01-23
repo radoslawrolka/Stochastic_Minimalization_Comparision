ebbefce4c098ea467a1d4f12be8dec63f793c2ff
Mateusz Kochelski <mkochelski@student.agh.edu.pl>
2024-01-23 17:39:05 +0100

    Visualization

--- src/Comparing_results.R
+++ src/Comparing_results.R
@@ -30,7 +30,55 @@ MS_Minimalize <- function(func, dim, points_quantity) {
   return (list(min(values), mean(counter)))
 }
 
+compare_MS_to_PRS = function(dim,func,numberOfRuns,NumberOfPoints)
+{
+  MS_resultsCounter = replicate(numberOfRuns,MS_Minimalize(func,dim,NumberOfPoints))
+  
+  MS_Counter = mean(as.numeric(MS_resultsCounter[2,]))
+  
+  MS_results = as.numeric(MS_resultsCounter[1,])
 
+  PRS_results = replicate(numberOfRuns,PRS(func,dim,NumberOfPoints*MS_Counter))
+  
+  t.test(MS_results,PRS_results,conf.level=0.95)
+  
+  
+  
+  
+  hist_plot = hist(MS_results, main = "Histogram dla algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+  hist_filename = paste0("dim",dim,"_MS_",sub("^make(.*)Function$","\\1", as.character(substitute(func))),"_his.png")
+  png(hist_filename)
+  plot(hist_plot)
+  dev.off()
+  
+  
+  
+  hist_plot = hist(PRS_results, main = "Histogram dla algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+  hist_filename = paste0("dim",dim,"_PRS_",sub("^make(.*)Function$","\\1", as.character(substitute(func))),"_his.png")
+  png(hist_filename)
+  plot(hist_plot)
+  dev.off()
+  
+  
+  box_plot = boxplot(MS_results, main = "Wykres pudełkowy dla algorytmu MS", ylab = "Zakres minimów")
+  box_filename = paste0("dim",dim,"_MS_",sub("^make(.*)Function$","\\1", as.character(substitute(func))),".png")
+  png(box_filename)
+  plot(box_plot)
+  dev.off()
+  
+  box_plot = boxplot(PRS_results, main = "Wykres pudełkowy dla algorytmu PRS", ylab = "Zakres minimów")
+  box_filename = paste0("dim",dim,"_PRS_",sub("^make(.*)Function$","\\1", as.character(substitute(func))),".png")
+  png(box_filename)
+  plot(box_plot)
+  dev.off()
+}
 
+compare_MS_to_PRS(2,makeAlpine01Function,10,10)
+#compare_MS_to_PRS(10,makeAlpine01Function,50,100)
+#compare_MS_to_PRS(20,makeAlpine01Function,50,100)
+
+#compare_MS_to_PRS(2,makeAckleyFunction,50,100)
+#compare_MS_to_PRS(10,makeAckleyFunction,50,100)
+#compare_MS_to_PRS(20,makeAckleyFunction,50,100)
 
 
